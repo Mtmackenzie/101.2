9a1d487a10af0bffbaf44b784c49eeed2569d404
Mtmackenzie <meganturley@gmail.com>
2019-03-21 01:27:13 +0000

    Refactoring methods

--- rps_bonus_final.rb
+++ rps_bonus_final.rb
@@ -1,5 +1,13 @@
 VALID_CHOICES = ['r', 'l', 'p', 'sc', 'sp']
 
+CHOICE_FULL_NAMES = { 'r' => ['rock'],
+                      'p' => ['paper'],
+                      'sc'=> ['scissors'],
+                      'l' => ['lizard'],
+                      'sp' => ['Spock'] }
+
+NUMBER_OF_ROUNDS_TO_WIN = 5
+
 winning_combos = { 'r' => ['l', 'sc'],
                    'p' => ['r', 'sp'],
                    'sc' => ['p', 'l'],
@@ -23,12 +31,12 @@ def welcome_message
   lizard eats paper, paper disproves Spock, Spock vaporizes rock,
   and, as it always has, rock crushes scissors."
   MSG
-  prompt('Play against the computer until 5 points to become the Grand Master!')
+  prompt("Play against the computer until #{NUMBER_OF_ROUNDS_TO_WIN} points to become the Grand Master!")
 end
 
 def input_options
   prompt(<<-MSG
-      Please input one of the following sets of letters:
+      Please enter one of the following sets of letters:
       r  => rock
       p  => paper
       sc  => scissors
@@ -39,12 +47,55 @@ def input_options
         )
 end
 
+def retrieve_user_input
+    input_options
+    gets.chomp.downcase
+end
+
 def display_invalid_input_message
   prompt('Not a valid entry! Try again!')
 end
 
+def user_choice_long(u)
+  CHOICE_FULL_NAMES.fetch(u).join
+end
+
+def computer_choice_long(c)
+  CHOICE_FULL_NAMES.fetch(c).join
+end
+
+def display_round_choices_message(u, c)
+  u = user_choice_long(u)
+  c = computer_choice_long(c)
+  puts "You chose #{u}, Computer chose #{c}."
+end
+
+def player_won_round?(user_win, computer_choice)
+  user_win.include?(computer_choice)
+end
+
+def display_points_message(u, c)
+  prompt("Points = You: #{u}, Computer: #{c}")
+end
+
+def user_won_match?(user_points)
+  user_points == NUMBER_OF_ROUNDS_TO_WIN
+end
+
+def computer_won_match?(computer_points)
+  computer_points == NUMBER_OF_ROUNDS_TO_WIN
+end
+
+def tie?(user_input, computer_choice)
+  user_input == computer_choice
+end
+
+def match_ended?(user_points, computer_points)
+  user_won_match?(user_points) || computer_won_match?(computer_points)
+end
+
 def display_play_again_message
-  prompt('Play again? Type Q to exit, or any other key to continue.')
+  prompt('Play again? Enter Q to exit, or enter any other key to continue.')
 end
 
 def play_again?(answer)
@@ -67,11 +118,9 @@ loop do # play again loop
   user_point_counter = 0
   computer_point_counter = 0
 
-  until user_point_counter >= 5 || computer_point_counter >= 5 # counter loop
-
+  until match_ended?(user_point_counter, computer_point_counter) # counter loop
     loop do # valid user entry loop
-      input_options
-      user_input = gets.chomp.downcase
+      user_input = retrieve_user_input
       break if valid_user_input?(user_input)
       display_invalid_input_message
     end # end valid user entry loop
@@ -80,33 +129,33 @@ loop do # play again loop
 
     user_win = winning_combos.fetch(user_input)
 
-    if user_win.include?(computer_choice)
+    if player_won_round?(user_win, computer_choice)
       user_point_counter += 1
       clear_screen
-      puts "You chose #{user_input}, Computer chose #{computer_choice}.
-          >>>>>>>>> You win.
-          You: #{user_point_counter}, Computer: #{computer_point_counter}"
-      break if user_point_counter >= 5
-    elsif user_input == computer_choice
+      display_round_choices_message(user_input, computer_choice)
+      puts ">>>>>>>>> You win."
+      display_points_message(user_point_counter, computer_point_counter)
+      break if user_point_counter == NUMBER_OF_ROUNDS_TO_WIN
+    elsif tie?(user_input, computer_choice)
       clear_screen
-      puts "Computer also chose #{computer_choice}.
-          >>>>>>>>> Tie.
-          You: #{user_point_counter}, Computer: #{computer_point_counter}"
-      break if computer_point_counter >= 5
+      display_round_choices_message(user_input, computer_choice)
+          puts "======== Tie."
+          display_points_message(user_point_counter, computer_point_counter)
+      break if computer_point_counter == NUMBER_OF_ROUNDS_TO_WIN
     else
       computer_point_counter += 1
       clear_screen
-      puts "You chose #{user_input}, Computer chose #{computer_choice}.
-         >>>>>>>>> Computer wins.
-          You: #{user_point_counter}, Computer: #{computer_point_counter}"
-      break if computer_point_counter >= 5
+      display_round_choices_message(user_input, computer_choice)
+        puts "~~~~~~~~ Computer wins."
+        display_points_message(user_point_counter, computer_point_counter)
+      break if computer_point_counter == NUMBER_OF_ROUNDS_TO_WIN
     end
 
   end # counter loop
 
-  if user_point_counter == 5
+  if user_point_counter == NUMBER_OF_ROUNDS_TO_WIN
     puts ">>>>>>>>> You are the Grand Master! >>>>>>>>>>"
-  elsif computer_point_counter == 5
+  elsif computer_point_counter == NUMBER_OF_ROUNDS_TO_WIN
     puts '~~~~~~ Computer is the Grand Master! ~~~~~~~'
   end
 
